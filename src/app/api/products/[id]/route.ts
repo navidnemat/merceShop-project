ef33befa303ab6cab2ca56937c41aec623ee1495
Navid.N <navidnemati888@gmail.com>
2025-08-01 08:51:13 +0330

    fix route bug

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,14 +1,13 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import db from '@/database/db.json'
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const product = db.newArrivals.find((item) => item.id.toString() === params.id)
+export async function GET(request: NextRequest, context: { params: { id: string } }) {
+  const id = context.params.id
+
+  const product = db.newArrivals.find((item) => item.id === id)
 
   if (!product) {
-    return NextResponse.json({ error: 'محصول پیدا نشد' }, { status: 404 })
+    return NextResponse.json({ error: 'Product not found' }, { status: 404 })
   }
 
   return NextResponse.json(product)
