8fdb2187550e09dca3628fc1c3019149919abf4f
Navid.N <navidnemati888@gmail.com>
2025-08-01 09:19:39 +0330

    fix bug 5

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -5,16 +5,34 @@ export async function GET(
   request: NextRequest,
   context: { params: { id: string } }
 ) {
-  const { id } = context.params;
+  const { id } = context.params
 
-  const product = db.newArrivals.find(item => item.id === id);
+  const product = db.newArrivals.find(item => item.id === id)
 
   if (!product) {
-    return NextResponse.json({ error: 'Product not found' }, { status: 404 });
+    return NextResponse.json({ error: 'Product not found' }, { status: 404 })
   }
 
-  return NextResponse.json(product);
+  return NextResponse.json(product)
 }
+
+// import { NextRequest, NextResponse } from 'next/server'
+// import db from '@/database/db.json'
+
+// export async function GET(
+//   request: NextRequest,
+//   context: { params: { id: string } }
+// ) {
+//   const { id } = context.params;
+
+//   const product = db.newArrivals.find(item => item.id === id);
+
+//   if (!product) {
+//     return NextResponse.json({ error: 'Product not found' }, { status: 404 });
+//   }
+
+//   return NextResponse.json(product);
+// }
 
 // import { NextRequest, NextResponse } from 'next/server'
 // import  db  from '@/database/db.json'
