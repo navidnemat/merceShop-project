57f97742ab43264428a18a8b3f22f69773a52086
Navid.N <navidnemati888@gmail.com>
2025-08-02 18:48:59 +0330

    fix feedbackitem bug

--- src/app/components/feedbackItem.tsx
+++ src/app/components/feedbackItem.tsx
@@ -5,7 +5,7 @@ interface cartProps {
 
 export default function FeedbackItem({text, name} : cartProps) {
     return (
-        <div className="bg-white/60 backdrop-blur-md rounded-2xl py-8 px-6 shadow-md border border-white/40 transition-all hover:shadow-xl hover:scale-[1.01] duration-300 my-10">
+        <div className="bg-white/60 backdrop-blur-md rounded-2xl py-8 px-6 shadow-md border border-white/40 transition-all hover:shadow-xl hover:scale-[1.01] duration-300 my-10 md:mx-0 mx-2 min-h-[220px]">
 
             
             <div className="flex items-center gap-1 mb-4">
